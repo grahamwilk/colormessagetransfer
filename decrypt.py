00f416f740ff228cca3c506492eaa3fdc52c8cc0
T0rt13 <mg6737lv@go.minnstate.edu>
2024-09-24 10:20:47 -0500

    Added a decrypt file
    -convert from image back to original text

---
+++ decrypt.py
@@ -0,0 +1,31 @@
+from PIL import Image
+import math
+
+
+def decrypt(path):
+    # find image based on path
+    image = Image.open(path)
+    width = image.width
+
+    # map the image to pixels
+    pixelMap = image.load()
+    message_list = []
+
+    # convert x,y and color information to a list
+    for x in range(width):
+        if x % 12 == 0:
+            for y in range(width):
+                if y % 12 == 0:
+                    for color in range(3):
+                        message_list.append(pixelMap[x,y][color])
+                    print(pixelMap[x,y])
+
+    # convert list back into original message
+    message_string = ""
+    for character in range(len(message_list)):
+        message_string += (chr(message_list[character] // 2))
+    print(message_string)
+
+
+if __name__ == "__main__":
+    decrypt("Abstract.PNG")
